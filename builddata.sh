a7ea2f2bbdd6996dca4fa4af3ea07686d9fc4548
AndrewCRMartin <andrew@bioinf.org.uk>
2020-11-25 17:19:30 +0000

    More improvements to builddata.sh

--- builddata.sh
+++ builddata.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 . ./findpdbabs.conf
 
-if [ ! -d $abtpldir ]; then
-    mkdir -p $abtpldir
-    if [ ! -d $abtpldir ]; then
-        echo "No permision to create $abtpldir"
-        exit 1
-    fi
-    echo "Created antibody template directory $abtpldir"
+if [ ! -f ./findpdbabs.conf ]; then
+    echo "You must create a configuration file: findpdbabs.conf"
+    exit 1
 fi
 
-mkdir -p $blastdir
-mkdir -p $dbmdir
+if [ $0 != "./builddata.sh" ]; then
+    echo "Script must be run in the source directory as ./builddata.sh"
+    exit 1
+fi
+
+function makedir
+{
+    dir=$1
+    msg=$2
+    if [ ! -d $dir ]; then
+        mkdir -p $dir
+        if [ ! -d $dir ]; then
+            echo "No permision to create $dir"
+            exit 1
+        fi
+        echo "Created $msg ($dir)"
+    else
+        echo "$msg already exists ($dir)"
+    fi
+}
+
+makedir $abtpldir "Antibody template directory"
+makedir $blastdir "Blast database directory"
+makedir $dbmdir   "DBM directory"
 
 (cd maketemplates; ./getpdbabseqs.pl)
 (cd maketemplates; ./getgbtcrs.pl)
-cat maketemplates/$tcrseqsfile $tplsubdir/*.faa > $tcrabsfile
-\rm -f maketemplates/$tcrseqsfile
+cat $tcrseqsfile $abtpldir/*.faa > $tcrabsfile
+\rm -f $tcrseqsfileq
 
